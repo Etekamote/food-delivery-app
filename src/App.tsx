00151ce4fd35d508bf484365b8b0ffbad6f4f033
Kacper <kacpermaciejewski1998@gmail.com>
2023-12-18 21:02:30 +0100

    project Initialization

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+
+export function App() {
+  const route = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<h1>404 Not found</h1>} />
+      </Route>
+    )
+  );
+
+  return (
+    <div className="App">
+      <RouterProvider router={route} />
+    </div>
+  );
+}
+
+export const Root = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
