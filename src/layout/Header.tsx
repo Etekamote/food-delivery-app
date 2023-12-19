f01919507cc14d202e7f029b532f5ccd33f92565
Kacper <kacpermaciejewski1998@gmail.com>
2023-12-19 22:04:02 +0100

    added cart length to header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { useCartStore } from "../stores/cartStore";
 function Header() {
+  const cartLength = useCartStore((state) => state.cart.length);
   return (
     <header className="text-center py-8 sticky top-0 bg-white z-50">
       <Link to="/">
@@ -18,7 +20,7 @@ function Header() {
           className=" text-2xl lg:text-4xl top-8 bg-orange-600 text-white rounded-full p-4 hover:scale-110 hover:cursor-pointer"
         />
         <span className="absolute flex -right-3 lg:-right-6 top-0 justify-center items-center bg-white rounded-full p-2 w-6 lg:w-10 h-6 lg:h-10 font-bold text-lg lg:text-xl">
-          0
+          {cartLength}
         </span>
       </span>
     </header>
