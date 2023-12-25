f160ece9e606b6dc7467a6da70774f01662f4028
Kacper <kacpermaciejewski1998@gmail.com>
2023-12-25 23:18:39 +0100

    Removed button from menu item and wrapped whole div in anchor tag

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -7,18 +7,20 @@ type MenuItemProps = {
 
 function MenuItem({ item }: MenuItemProps) {
   return (
-    <div className="mx-4 bg-white border-orange-500 border w-full lg:w-[48%] flex justify-between  p-4 ">
-      <img
-        src={item.image || `https://via.placeholder.com/150`}
-        alt={item.name}
-        className="h-[100px] w-[35%] "
-      />
-      <section className="w-[60%]">
-        <h3 className="text-orange-500 uppercase">{item.name}</h3>
-        <p className="text-lg">{item.description}</p>
-        <div className="flex justify-between mt-4 w-full">${item.price}</div>
-      </section>
-    </div>
+    <a href={`#${item.id}`}>
+      <div className="mx-4 bg-white border-orange-500 border w-full lg:w-[48%] flex justify-between  p-4 ">
+        <img
+          src={item.image || `https://via.placeholder.com/150`}
+          alt={item.name}
+          className="h-[100px] w-[35%] "
+        />
+        <section className="w-[60%]">
+          <h3 className="text-orange-500 uppercase">{item.name}</h3>
+          <p className="text-lg">{item.description}</p>
+          <div className="flex justify-between mt-4 w-full">${item.price}</div>
+        </section>
+      </div>
+    </a>
   );
 }
 
