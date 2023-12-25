b9e2ae44d4039b627b82d29b73c7b748e60cff05
Kacper <kacpermaciejewski1998@gmail.com>
2023-12-25 23:18:26 +0100

    Removed button from menu item and wrapped whole div in anchor tag

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -16,12 +16,7 @@ function MenuItem({ item }: MenuItemProps) {
       <section className="w-[60%]">
         <h3 className="text-orange-500 uppercase">{item.name}</h3>
         <p className="text-lg">{item.description}</p>
-        <div className="flex justify-between mt-4 w-full">
-          <span>${item.price}</span>
-          <button className="bg-orange-500 text-white px-4 py-2 text-lg ">
-            <Link to={`/food/${item.id}`}>Order</Link>
-          </button>
-        </div>
+        <div className="flex justify-between mt-4 w-full">${item.price}</div>
       </section>
     </div>
   );
