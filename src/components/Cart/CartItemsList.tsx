10ca3bae965241fd6005656a63b6426368a4c8ac
Kacper <kacpermaciejewski1998@gmail.com>
2023-12-28 23:35:21 +0100

    changes in cart

--- src/components/Cart/CartItemsList.tsx
+++ src/components/Cart/CartItemsList.tsx
@@ -7,6 +7,7 @@ function CartItemsList() {
 
   return (
     <>
+      {cartItems.length === 0 && <p>No items in cart</p>}
       <section className="w-[30%] flex flex-col gap-8">
         {cartItems.map((item) => (
           <CartItem key={item.cartId} item={item} />
